f107c6fcc4672d10b9a54af23699a0e466c95234
Shashank <shashank00098@gmail.com>
2023-03-28 07:14:40 +0000

    props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import "./styles.css";
 import { SumCard } from "./SumCard";
-function EmployeeCard(){
-  return(
-    <div className="App"> 
+function EmployeeCard() {
+  return (
+    <div className="App">
       <h2> Shashank</h2>
-      <p> <span style ={{color:"red"}}>Designation</span>  : Senior Software Engineer </p>
-      <p> <span style={{color:"blue"}}> Experience</span> : <span style={{fontSize:"18px"}}>5 years</span>  </p>
+      <p>
+        {" "}
+        <span style={{ color: "red" }}>Designation</span> : Senior Software
+        Engineer{" "}
+      </p>
+      <p>
+        {" "}
+        <span style={{ color: "blue" }}> Experience</span> :{" "}
+        <span style={{ fontSize: "18px" }}>5 years</span>{" "}
+      </p>
     </div>
-  )
+  );
 }
 
 export default function App() {
+  console.log("done");
   return (
-    <div className="App"> 
-    <SumCard/>    
-   <EmployeeCard/>
+    <div className="App">
+      <SumCard firstNumber={6} secondNumber={8} />
+      <EmployeeCard />
     </div>
-   
   );
 }
--- src/SumCard.jsx
+++ src/SumCard.jsx
@@ -1,12 +1,13 @@
-export function SumCard()  {
-    return(
-         <div className="App">
-      <h2 style={{color:"green"}}> Sum of Numbers</h2>
-      <p style={{fontSize:"20px"}}> First number : 5 </p>
-      <p> Second number : 5 </p>
-      <p> Sum : 20 </p>
-    </div>
-    )
+export function SumCard(props)  {
+  console.log(props);
+  console.log({props})
+    // return(
+    //      <div className="App">
+    //   <h2> Sum of Numbers</h2>
+    //   <p> First number : {firstNumber} </p>
+    //   <p> Second number : {secondNumber} </p>
+    //   <p> Sum : {firstNumber+secondNumber} </p>
+    // </div>
+    // )
 }
 
- 
