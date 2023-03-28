8de2eb576c519f506276c9eb7b8cb73d1ad9fe7a
Shashank <shashank00098@gmail.com>
2023-03-28 03:27:43 +0000

    EX01_2 ways to import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./styles.css";
+import { SumCard } from "./SumCard";
+function EmployeeCard(){
+  return(
+    <div className="App"> 
+      <h2> Shashank</h2>
+      <p> Designation : Senior Software Engineer </p>
+      <p> Experience : 5 years </p>
+    </div>
+  )
+}
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App"> 
+    <SumCard/>    
+   <EmployeeCard/>
     </div>
+   
   );
 }
---
+++ src/SumCard.jsx
@@ -0,0 +1,10 @@
+export function SumCard()  {
+    return(
+         <div className="App">
+      <h2> Sum of Numbers</h2>
+      <p> First number : 5 </p>
+      <p> Second number : 5 </p>
+      <p> Sum : 10 </p>
+    </div>
+    )
+}
