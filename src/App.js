6e20d4cf1baf9edc13fa3f1f5e407086d5d4bd99
Shashank <shashank00098@gmail.com>
2023-03-28 09:46:19 +0000

    AC card

--- src/App.js
+++ src/App.js
@@ -18,12 +18,34 @@ function EmployeeCard() {
   );
 }
 
+const product = {
+  id: "AC1023",
+  name: "Air Conditioner",
+  price: 29600,
+  specification: "1 Ton, 4 Star Rating",
+  warranty: "5 Years Compressor Warranty"
+};
+
+function AC_Card({name,id,price,specification,warranty}){
+  return(
+    <div className="header-name">
+      <h2 style={{color:"yellow",fontSize:"20px"}}>{id} {name}</h2>
+      <p>Price: INR {price} </p>
+      <p>Specification: {specification}</p>
+      <p>Warranty: {warranty} years</p>
+    </div>
+  )
+
+}
+
 export default function App() {
   console.log("done");
   return (
     <div className="App">
       <SumCard firstNumber={6} secondNumber={8} />
       <EmployeeCard />
+      <AC_Card product={{product}}/>
+
     </div>
   );
 }
