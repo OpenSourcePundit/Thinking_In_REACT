9c6b1983e3563676e22597f0bf29194a66dcac3a
Shashank <shashank00098@gmail.com>
2023-03-28 07:16:09 +0000

    Props_Usage

--- src/SumCard.jsx
+++ src/SumCard.jsx
@@ -1,13 +1,12 @@
-export function SumCard(props)  {
-  console.log(props);
-  console.log({props})
-    // return(
-    //      <div className="App">
-    //   <h2> Sum of Numbers</h2>
-    //   <p> First number : {firstNumber} </p>
-    //   <p> Second number : {secondNumber} </p>
-    //   <p> Sum : {firstNumber+secondNumber} </p>
-    // </div>
-    // )
+export function SumCard({firstNumber,secondNumber})  {
+
+    return(
+         <div className="App">
+      <h2> Sum of Numbers</h2>
+      <p> First number : {firstNumber} </p>
+      <p> Second number : {secondNumber} </p>
+      <p> Sum : {firstNumber+secondNumber} </p>
+    </div>
+    )
 }
 
